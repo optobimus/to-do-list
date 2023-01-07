2c1caf661c3fce4542dd1ace22b45044710789ad
Tobias <113835194+optobimus@users.noreply.github.com>
2023-01-07 11:34:48 +0100

    Add basic logic for switching between pages Create modules to separate DOM and logic elements. Minor styling changes.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-import './styles.css'
+import './styles.css';
+import {createMain} from './dom';
+import {todo, project} from './logic';
+
+const navBtns = document.querySelectorAll(".navBtn");
+
+navBtns.forEach((button) => 
+    button.addEventListener('click', (e) => {
+        navBtns.forEach((button) => {
+            button.classList.remove("active");
+        });
+        button.classList.add("active");
+        createMain(button.querySelector("svg").cloneNode(true), button.textContent.trim());
+    })
+);
