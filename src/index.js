8a0e9f6ff925c98b16535b6a02cb306d435730b6
Tobias <113835194+optobimus@users.noreply.github.com>
2023-01-07 15:24:03 +0100

    Make DOM functionality to add projects to sidebar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './styles.css';
-import {createMain, createProject, closeOverlay} from './dom';
+import {createMain, createProject, closeOverlay, displayProjects} from './dom';
 import {todo, project} from './logic';
 
 const navBtns = document.querySelectorAll(".navBtn");
-const defaultProject = project("default");
+const defaultProject = project("defaultProject");
+const projects = [];
+projects.push(defaultProject);
 
 navBtns.forEach((button) => 
     button.addEventListener('click', (e) => {
@@ -18,8 +20,20 @@ navBtns.forEach((button) =>
 const projectBtn = document.querySelector(".addProjectBtn");
 projectBtn.addEventListener("click", (e) => {
     createProject();
+    const inputField = document.querySelector(".inputField")
+
     const cancelBtn = document.querySelector(".cancelBtn");
     cancelBtn.addEventListener("click", (e) => {
+        closeOverlay();
+    });
+
+    const submitBtn = document.querySelector(".confirmBtn");
+    submitBtn.addEventListener("click", (e) => {
+        if (projects[0].getTitle() == "defaultProject") {
+            projects.pop();
+        }
+        projects.push(project(inputField.value));
+        displayProjects(projects);
         closeOverlay();
     });
 });
